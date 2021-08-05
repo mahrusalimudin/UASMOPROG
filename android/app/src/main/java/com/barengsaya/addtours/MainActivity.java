1561347bab24937c99f5ea44c2c36c4d0cf688cb
mahrusalimudin <83571985+mahrusalimudin@users.noreply.github.com>
2021-08-05 15:05:06 +0700

    update package

--- android/app/src/main/java/com/barengsaya/addtours/MainActivity.java
+++ android/app/src/main/java/com/barengsaya/addtours/MainActivity.java
@@ -1,4 +1,4 @@
-package com.raja;
+package com.barengsaya.addtours;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "raja";
+    return "addtours";
   }
 }
--- ios/addtoursTests/addtoursTests.m
+++ ios/addtoursTests/addtoursTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rajaTests : XCTestCase
+@interface addtoursTests : XCTestCase
 
 @end
 
-@implementation rajaTests
+@implementation addtoursTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
